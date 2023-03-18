f62ca7382c3bb8b1a2a954572629f2cba5eda517
Khreiv <khreiv@gmx.com>
2023-03-18 03:11:37 +0100

    Mathok Calculator by Khreiv

---
+++ README.md
@@ -0,0 +1,21 @@
+# MATHOK Calculator
+
+This is a simple calculator application built with Python and the Tkinter library. 
+The calculator allows users to perform basic arithmetic operations, as well as some advanced mathematical functions.
+
+## Key Features
+
+- Perform basic arithmetic operations: addition, subtraction, multiplication, and division.
+- Use advanced mathematical functions: square root, exponentiation, logarithm, and percentage.
+- Input numbers and operations using both the on-screen buttons and the keyboard.
+- Keyboard shortcuts for clearing, deleting the last character, and evaluating the expression.
+- Prevents moving the cursor within the input field to ensure that input is always at the end of the expression.
+
+## How to Interact with the Application
+
+- Input numbers and operators by either clicking the on-screen buttons or typing them using the keyboard.
+- Press the Enter key to evaluate the expression and display the result.
+- Press the Escape key to clear the input field.
+- Press the Backspace key to delete the last character in the input field.
+
+Please note that the application will prevent you from moving the cursor within the input field. All input will be added at the end of the expression.
---
+++ main.py
@@ -0,0 +1,111 @@
+from tkinter import *
+from math import *
+
+
+def key_handler(event):
+    if event.keysym.isdigit():
+        click(event.keysym)
+    elif event.keysym in ('plus', 'minus', 'asterisk', 'slash'):
+        click(event.keysym.replace('plus', '+').replace('minus', '-').replace('asterisk', '*').replace('slash', '/'))
+    elif event.keysym == 'period':
+        click('.')
+    elif event.keysym == 'Return':
+        operation()
+    elif event.keysym == 'Escape':
+        clean()
+    elif event.keysym == 'BackSpace':
+        delete_last()
+
+
+def delete_last():
+    global OPERATOR
+    OPERATOR = OPERATOR[:-1]
+    input_text.set(OPERATOR)
+
+
+def disable_arrow_keys(_):
+    return "break"
+
+
+root = Tk()
+root.title('mathok')
+root.geometry('392x600')
+root.iconbitmap('images/mathok.ico')
+root.bind('<Key>', key_handler)
+
+input_text = StringVar()
+OPERATOR = ''
+
+input_entry = Entry(root, font=('arial', 20), width=22, textvariable=input_text,
+                    bd=20, insertbackground='powder blue', bg='powder blue', justify='right')
+
+input_entry.place(x=10, y=60)
+
+input_entry.focus_set()
+input_entry.bind('<Left>', disable_arrow_keys)
+input_entry.bind('<Right>', disable_arrow_keys)
+
+
+# NUMBERS
+def click(num):
+    global OPERATOR
+    OPERATOR = OPERATOR + str(num)
+    input_text.set(OPERATOR)
+
+
+# OPERATORS
+def operation():
+    global OPERATOR
+    try:
+        op = str(eval(OPERATOR))
+        input_text.set(op)
+    except:
+        input_text.set('3RR0R')
+        OPERATOR = ''
+
+
+# CLEAR
+def clean():
+    global OPERATOR
+    OPERATOR = ''
+    input_text.set('0')
+
+
+# BUTTON SIZE
+btn_width = 11
+btn_height = 3
+
+# X 0 BUTTONS
+Button(root, text='+', width=btn_width, height=btn_height, command=lambda: click('+')).place(x=17, y=180)
+Button(root, text='-', width=btn_width, height=btn_height, command=lambda: click('-')).place(x=17, y=240)
+Button(root, text='*', width=btn_width, height=btn_height, command=lambda: click('*')).place(x=17, y=300)
+Button(root, text='/', width=btn_width, height=btn_height, command=lambda: click('/')).place(x=17, y=360)
+Button(root, text='\u221a', width=btn_width, height=btn_height, command=lambda: click('sqrt')).place(x=17, y=420)
+Button(root, text='%', width=btn_width, height=btn_height, command=lambda: click('%')).place(x=17, y=480)
+
+# X 1 BUTTONS
+Button(root, text='7', width=btn_width, height=btn_height, command=lambda: click(7)).place(x=105, y=180)
+Button(root, text='4', width=btn_width, height=btn_height, command=lambda: click(4)).place(x=105, y=240)
+Button(root, text='1', width=btn_width, height=btn_height, command=lambda: click(1)).place(x=105, y=300)
+Button(root, text='0', width=btn_width, height=btn_height, command=lambda: click(0)).place(x=105, y=360)
+Button(root, text='EXP', width=btn_width, height=btn_height, command=lambda: click('**')).place(x=105, y=420)
+Button(root, text='In', width=btn_width, height=btn_height, command=lambda: click('log')).place(x=105, y=480)
+
+# X 2 BUTTONS
+Button(root, text='8', width=btn_width, height=btn_height, command=lambda: click(8)).place(x=193, y=180)
+Button(root, text='5', width=btn_width, height=btn_height, command=lambda: click(5)).place(x=193, y=240)
+Button(root, text='2', width=btn_width, height=btn_height, command=lambda: click(2)).place(x=193, y=300)
+Button(root, text=',', width=btn_width, height=btn_height, command=lambda: click('.')).place(x=193, y=360)
+Button(root, text='(', width=btn_width, height=btn_height, command=lambda: click('(')).place(x=193, y=420)
+Button(root, text='\u03C0', width=btn_width, height=btn_height, command=lambda: click('pi')).place(x=193, y=480)
+
+# X 3 BUTTONS
+Button(root, text='9', width=btn_width, height=btn_height, command=lambda: click(9)).place(x=282, y=180)
+Button(root, text='6', width=btn_width, height=btn_height, command=lambda: click(6)).place(x=282, y=240)
+Button(root, text='3', width=btn_width, height=btn_height, command=lambda: click(3)).place(x=282, y=300)
+Button(root, text='C', width=btn_width, height=btn_height, command=clean).place(x=282, y=360)
+Button(root, text=')', width=btn_width, height=btn_height, command=lambda: click(')')).place(x=282, y=420)
+Button(root, text='=', width=btn_width, height=btn_height, command=operation).place(x=282, y=480)
+
+# TK LOOP
+root.mainloop()
